27ce23d165095147a176375d1b53362c7a60291c
Nickolashka52 <theinfothere@gmail.com>
2025-01-26 21:33:36 +0300

    Сделал 6-ую домашку

--- HomeWork_1/scripts.js
+++ HomeWork_1/scripts.js
@@ -16,4 +16,58 @@ function guessTheNumber() {
             alert(`Поздравляем! Вы угадали число ${randomNumber} за ${attempts} попыток!`);
         }
     }
-}
+};
+
+// Игра простая арифметика
+function simpleArithmetic() {
+
+    function getRandomNumber(max) {
+        return Math.floor(Math.random() * max) + 1;
+    };
+
+    function generateTask() {
+        const operations = ['+', '-', '*', '/'];
+        const num1 = getRandomNumber(10);
+        const num2 = getRandomNumber(10);
+        const operation = operations[getRandomNumber(operations.length) - 1];
+
+        if (operation === '/' && num2 === 0) {
+            return generateTask();
+        };
+
+        return {
+            task: `${num1} ${operation} ${num2}`,
+            operation: operation,
+            num1: num1,
+            num2: num2
+        };
+    };
+
+    function calculateAnswer(num1, num2, operation) {
+        switch (operation) {
+            case "+":
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                return num1 / num2;
+            default:
+                return null;
+        }
+    };
+
+    const taskData = generateTask();
+    const userAnswer = Number(prompt(`Решите задачу: ${taskData.task}`));
+
+    if (userAnswer !== null) {
+        const correctAnswer = Number(calculateAnswer(taskData.num1, taskData.num2, taskData.operation));
+
+        if (userAnswer === correctAnswer) {
+            alert("Правильно!");
+        } else {
+            alert(`Ошибка. Правильный ответ: ${correctAnswer}`);
+        }
+    };
+};
