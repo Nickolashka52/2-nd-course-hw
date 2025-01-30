7d3534593f4ba49396cfb2dd6f418db42b3ee961
Nickolashka52 <theinfothere@gmail.com>
2025-01-30 15:20:26 +0300

    Сделал 7-ую домашку

---
+++ HomeWork_1/hw-7.js
@@ -0,0 +1,108 @@
+//Задание №1
+
+const stringTask1 = 'js';
+console.log(stringTask1.toUpperCase());
+
+//Задание №2
+
+const arrStringsTask2 = ['Барбарисовый маринад', 'Фиолетовый домкрат', 'Железная доска'];
+const stringTask2 = 'фиолетовый';
+arrStringsTask2.forEach((element) => {
+    if (element.toLowerCase().startsWith(stringTask2.toLowerCase())) {
+        console.log(element);
+    }
+});
+
+//Задание №3
+
+const numberTask3 = 32.58884;
+console.log(Math.floor(numberTask3));
+console.log(Math.ceil(numberTask3));
+console.log(Math.round(numberTask3));
+
+//Задание №4
+
+const arrTask4 = [52, 53, 49, 77, 21, 32];
+console.log(Math.max(...arrTask4));
+console.log(Math.min(...arrTask4));
+
+//Либо так:
+
+console.log(Math.max(52, 53, 49, 77, 21, 32));
+console.log(Math.min(52, 53, 49, 77, 21, 32));
+
+//Задание №5
+
+const numberTask5 = Math.ceil(Math.random() * 10);
+console.log(numberTask5);
+
+//Задание №6
+
+function generateRandomArray(num) {
+    let length = Math.floor(num / 2);
+    const randomArray = [];
+    for (let i = 0; i < length; i++) {
+        let randomNum = Math.floor(Math.random() * (num + 1));
+        randomArray.push(randomNum);
+    }
+    return randomArray;
+}
+console.log(generateRandomArray(14));
+
+//Задание №7
+
+function getRandomNumberInRange(min, max) {
+
+    let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    return randomNum;
+}
+console.log(getRandomNumberInRange(3, 19));
+console.log(getRandomNumberInRange(-3, 8));
+
+//Задание №8
+
+let currentDateTask8 = new Date();
+console.log(currentDateTask8);
+
+//Задание №9
+
+let currentDateTask9 = new Date();
+console.log("Текущая дата: " + currentDateTask9.toLocaleDateString());
+
+let currentDayTask9 = currentDateTask9.getDate();
+currentDateTask9.setDate(currentDayTask9 + 73);
+
+console.log("Дата через 73 дня: " + currentDateTask9.toLocaleDateString());
+
+//Задание №10
+
+function formatDateTime(date) {
+
+    const months = [
+        "января", "февраля", "марта", "апреля",
+        "мая", "июня", "июля", "августа",
+        "сентября", "октября", "ноября", "декабря"
+    ];
+
+    const daysOfWeek = [
+        "воскресенье", "понедельник", "вторник",
+        "среда", "четверг", "пятница", "суббота"
+    ];
+
+    let day = date.getDate();
+    let month = months[date.getMonth()];
+    let year = date.getFullYear();
+    let dayOfWeek = daysOfWeek[date.getDay()];
+
+    let hours = String(date.getHours());
+    let minutes = String(date.getMinutes());
+    let seconds = String(date.getSeconds());
+
+    let datePart = `Дата: ${day} ${month} ${year} — это ${dayOfWeek}.`;
+    let timePart = `Время: ${hours}:${minutes}:${seconds}`;
+
+    return `${datePart}\n${timePart}`;
+}
+
+let currentDate = new Date();
+console.log(formatDateTime(currentDate));
--- HomeWork_1/scripts.js
+++ HomeWork_1/scripts.js
@@ -1,4 +1,4 @@
-// Игра угадай число
+// Игра "Угадай число"
 function guessTheNumber() {
     const randomNumber = Math.floor(Math.random() * 100) + 1;
     let userNumber = 0;
@@ -18,7 +18,7 @@ function guessTheNumber() {
     }
 };
 
-// Игра простая арифметика
+// Игра "Простая арифметика"
 function simpleArithmetic() {
 
     function getRandomNumber(max) {
@@ -70,4 +70,72 @@ function simpleArithmetic() {
             alert(`Ошибка. Правильный ответ: ${correctAnswer}`);
         }
     };
+};
+
+// Игра "Переверни текст"
+function turnTheTextOver() {
+
+    let userInput = prompt("Введите текст, который нужно перевернуть:");
+
+    if (userInput !== null) {
+        const reversed = userInput.split('').reverse().join('').toLowerCase();
+
+        if (/[.!?]/.test(reversed)) {
+            let firstChar = reversed.charAt(0);
+            let reversedWithTransferredPunctuationMark = reversed.substring(1) + firstChar;
+            let reversedWithTransferredPunctuationMarkCapitalized = reversedWithTransferredPunctuationMark.charAt(0).toUpperCase() + reversedWithTransferredPunctuationMark.slice(1);
+            alert("Перевёрнутый текст: " + reversedWithTransferredPunctuationMarkCapitalized);
+
+        } else {
+
+            let reversedCapitalized = reversed.charAt(0).toUpperCase() + reversed.slice(1);
+            alert("Перевёрнутый текст: " + reversedCapitalized);
+
+        }
+
+    } else {
+        alert("Вы не ввели текст.");
+    }
+
+}
+
+// Игра "Викторина"
+function simpleQuiz() {
+
+    const quiz = [
+        {
+            question: "Какой цвет у неба?",
+            options: ["1. Красный", "2. Синий", "3. Зеленый"],
+            correctAnswer: 2
+        },
+        {
+            question: "Сколько дней в неделе?",
+            options: ["1. Шесть", "2. Семь", "3. Восемь"],
+            correctAnswer: 2
+        },
+        {
+            question: "Сколько у человека пальцев на одной руке?",
+            options: ["1. Четыре", "2. Пять", "3. Шесть"],
+            correctAnswer: 2
+        }
+    ];
+
+    let score = 0;
+
+    for (let i = 0; i < quiz.length; i++) {
+        const currentQuestion = quiz[i];
+        let optionsText = currentQuestion.options.join('\n');
+        let userAnswer = Number(prompt(currentQuestion.question + '\n' + optionsText));
+
+        if (userAnswer === null) {
+            alert("Вы вышли из викторины.");
+            return;
+        }
+
+        if (userAnswer === currentQuestion.correctAnswer) {
+            score++;
+        }
+    }
+
+    alert("Вы ответили правильно на " + score + " из " + quiz.length + " вопросов.");
 };
