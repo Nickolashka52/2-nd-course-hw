2935b3979cedcb6dbebbe976fd86cc2d088899ee
Nickolashka52 <theinfothere@gmail.com>
2025-02-08 22:55:37 +0300

    Сделал 9-ую домашку

---
+++ HomeWork_1/hw-9.js
@@ -0,0 +1,76 @@
+//Задание №1
+
+const titleTask1El = document.querySelector('.title_task1');
+const buttonTask1El = document.querySelector('.button_task1');
+let isHeaderVisible = true;
+
+buttonTask1El.addEventListener('click', function () {
+    if (isHeaderVisible) {
+        titleTask1El.remove();
+        //      header.style.display = 'none';     ***       2-ой вариант
+    } else {
+        document.body.insertBefore(titleTask1El, buttonTask1El);
+        //      header.style.display = 'block';     ***      2-ой вариант
+    }
+
+    isHeaderVisible = !isHeaderVisible;
+});
+
+//Задание №2
+
+const textTask2El = document.querySelector('.text_task2');
+const buttonTask2El = document.querySelector('.button_task2');
+
+buttonTask2El.addEventListener('click', function () {
+    textTask2El.style.color = 'red';
+});
+
+//Задание №3
+
+const titleTask3El = document.querySelector('.title_task3');
+const buttonTask3El = document.querySelector('.button_task3');
+
+buttonTask3El.addEventListener('click', function () {
+    titleTask3El.textContent = 'Привет, мир!';
+});
+
+//Задание №4
+
+const allDescriptionsTask4 = document.querySelectorAll('.description_task4');
+const buttonTask4El = document.querySelector('.button_task4');
+
+buttonTask4El.addEventListener('click', function () {
+    allDescriptionsTask4.forEach(description => {
+        description.textContent = 'Измененный текст';
+    });
+});
+
+//Задание №5
+
+const allDescriptionsTask5 = document.querySelectorAll('.description_task5');
+const buttonTask5El = document.querySelector('.button_task5');
+
+buttonTask5El.addEventListener('click', function () {
+    allDescriptionsTask5.forEach(description => {
+        description.textContent = 'Измененный текст';
+    });
+});
+
+//Задание №6
+
+const newParagraph = document.createElement('div');
+newParagraph.textContent = 'Новый параграф';
+const buttonTask6El = document.querySelector('.button_task6');
+
+buttonTask6El.addEventListener('click', function () {
+    document.body.appendChild(newParagraph);
+});
+
+//Задание №7
+
+const descriptionTask7El = document.querySelector('.description_task7');
+const buttonTask7El = document.querySelector('.button_task7');
+
+buttonTask7El.addEventListener('click', function () {
+    descriptionTask7El.remove();
+});
